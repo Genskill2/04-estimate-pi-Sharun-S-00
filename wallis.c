1dee804c10b5c438c8eb0e2ed704184e71a6154c
Sharun-S-00 <84717172+Sharun-S-00@users.noreply.github.com>
2021-06-04 12:21:18 +0530

    Update wallis.c
    Assignment submission

--- wallis.c
+++ wallis.c
@@ -1,10 +1,37 @@
-float wallis_pi(int n)
- {
-    float s=1.0;
-    for (int i=1; i<=n; i++)
-    {
-        s=s*(4*i*i)/((4*i*i)-1);
-    }
-   return (2*s); 
- }
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
+float wallis_pi(int);
+
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
+float wallis_pi(int n)
+{
+ float product;
+ product=2.0;
+ int i,j;
+ for(i=1;i<n+1;i++)
+ {
+ product = product / ((4 * i * i) - 1);
+ }
+ return product;
+}
